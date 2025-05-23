0d214dadf3fd2f064ad01cf7d6be633acc114980
daehyuh <rkdeown10@naver.com>
2025-05-24 07:32:25 +0900

    add cors

---
+++ src/main/java/com/example/daehyunbackend/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.example.daehyunbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins(
+//                // TODO 프로덕션 환경 자동 감지
+//                /* DEV */
+                "http://localhost", // backend
+                "http://localhost:5173", // frontend
+//                /* PRODUCTION */
+                "https://hufsnc.com", // backend
+                "https://대현.com" // frontend
+            )
+                .allowedOrigins("*")
+                .allowedMethods("*"); // 필요한 메서드 추가
+    }
+}
