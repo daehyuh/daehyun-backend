d565b1c1e5b2d911700ed17b5ad021cb5cc5f01e
rkdeown10 <rkdeown10@naver.com>
2025-05-26 02:21:11 +0900

    cors csrf disable

--- src/main/java/com/example/daehyunbackend/config/WebConfig.java
+++ src/main/java/com/example/daehyunbackend/config/WebConfig.java
@@ -9,18 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedOrigins(
-                        "https://daehyun.dev",
-                        "https://api.daehyun.dev",
-                        "http://localhost:5173",
-                        "http://localhost:8080",
-                        "https://daehyun.dev",
-                        "https://daehyun-frontend.vercel.app/"
-                )
-                .allowedMethods("**")
-                .allowCredentials(true)
-                .allowedMethods("**"); // 모든 HTTP 메서드 허용
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 프론트 주소
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
