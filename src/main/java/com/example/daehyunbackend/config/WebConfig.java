2f146ea106dbad00459dc712d11152560390d9c6
rkdeown10 <rkdeown10@naver.com>
2025-05-25 23:22:32 +0900

    CORS

--- src/main/java/com/example/daehyunbackend/config/WebConfig.java
+++ src/main/java/com/example/daehyunbackend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(final CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("**") // 모든 출처 허용
+                .allowedOrigins("https://daehyun.dev") // 프론트 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .allowedMethods("**"); // 모든 HTTP 메서드 허용
