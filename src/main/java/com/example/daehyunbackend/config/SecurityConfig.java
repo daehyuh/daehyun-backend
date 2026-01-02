3f4667d96b3d1fa108e5f281051bfd277e955b62
rkdeown10 <rkdeown10@naver.com>
2026-01-03 02:31:00 +0900

    이미지 수정

--- src/main/java/com/example/daehyunbackend/config/SecurityConfig.java
+++ src/main/java/com/example/daehyunbackend/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.daehyunbackend.config;
 
 import com.example.daehyunbackend.unit.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +19,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @RequiredArgsConstructor
